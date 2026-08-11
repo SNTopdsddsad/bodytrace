55997aeb9b9de7574e54894a5984391d04685e29
丹青 <xuwudi404@gmail.com>
2026-08-11 15:04:35 +0800

    Initial Commit

---
+++ bodycheck/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  bodycheck
+//
+//  Created by xuwudi on 2026/8/11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ bodycheck/bodycheckApp.swift
@@ -0,0 +1,17 @@
+//
+//  bodycheckApp.swift
+//  bodycheck
+//
+//  Created by xuwudi on 2026/8/11.
+//
+
+import SwiftUI
+
+@main
+struct bodycheckApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
